c9a0ef967a47f1618475a58c05b886ea6a1a3127
shaheryarali98 <shaheryarali446@gmail.com>
2025-11-26 07:41:54 +0500

    fix

--- frontend/src/components/student/StudentTermsAndConditions.tsx
+++ frontend/src/components/student/StudentTermsAndConditions.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import api from '../../lib/api'
 
 const StudentTermsAndConditions = () => {
   const [agreed, setAgreed] = useState(false)
